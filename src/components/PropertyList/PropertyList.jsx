0c14da9bfdb712ed0cb14f06d81a89c585eae7c6
ArturoEstrada0 <116518245+ArturoEstrada0@users.noreply.github.com>
2024-01-05 02:30:35 -0600

    Boton favoritos

--- src/components/PropertyList/PropertyList.jsx
+++ src/components/PropertyList/PropertyList.jsx
@@ -20,23 +20,43 @@ import {
   AreaChartOutlined,
   SearchOutlined,
   EnvironmentOutlined,
+  HeartFilled,
+  HeartOutlined,
 } from "@ant-design/icons";
 import "./PropertyList.css";
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  doc,
+  updateDoc,
+  increment
+} from "firebase/firestore";
 import { firestore } from "../firebase/firebase";
 import { Link } from "react-router-dom";
+
+import { useAuth } from "../../context/AuthContext";
 
 const { Content } = Layout;
 const { Option } = Select;
 const { Title, Text } = Typography;
 
 const PropertyList = ({ onPropertyClick }) => {
+  const [propertyClickCount, setPropertyClickCount] = useState(0);
   const [filterType, setFilterType] = useState("all");
   const [filterPrice, setFilterPrice] = useState([0, 1000000]);
   const [filterState, setFilterState] = useState("all");
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [userAuthenticated, setUserAuthenticated] = useState(false);
 
-  const [minPrice, setMinPrice] = useState('');
-  const [maxPrice, setMaxPrice] = useState('');
+  const [minPrice, setMinPrice] = useState("");
+  const [maxPrice, setMaxPrice] = useState("");
+
+  const { isAuthenticated, user } = useAuth(); // Agrega esta línea para obtener el estado de autenticación y el usuario actual
+
+  useEffect(() => {
+    // Verifica si el usuario está autenticado
+    setUserAuthenticated(isAuthenticated);
+  }, [isAuthenticated]);
 
   const handleMinPriceChange = (e) => {
     setMinPrice(e.target.value);
@@ -94,12 +114,64 @@ const PropertyList = ({ onPropertyClick }) => {
     fetchProperties();
   }, []);
 
+  const handlePropertyClick = async (propertyId) => {
+    // Incrementa el contador local
+    setPropertyClickCount(propertyClickCount + 1);
+
+    // Referencia al documento de la propiedad en Firestore
+    const propertyDocRef = doc(
+      collection(firestore, "propiedades"),
+      propertyId
+    );
+
+    // Actualiza el contador en Firestore
+    try {
+      await updateDoc(propertyDocRef, {
+        clickCount: increment(1), // Incrementa el valor actual en 1
+      });
+
+      // Llama a la función original pasada como prop (si es necesario)
+      if (onPropertyClick) {
+        onPropertyClick(propertyId);
+      }
+    } catch (error) {
+      console.error("Error al actualizar el contador en Firestore:", error);
+    }
+  };
+
+  const toggleFavorite = async (propertyId) => {
+    try {
+      if (!isAuthenticated) {
+        // Si el usuario no está autenticado, redirige a la página de inicio de sesión
+        // o muestra un mensaje para iniciar sesión
+        return;
+      }
+
+      // Actualiza el estado local de favoritos
+      setIsFavorite(!isFavorite);
+
+      // Referencia al documento de la propiedad en Firestore
+      const propertyDocRef = doc(collection(firestore, "propiedades"), propertyId);
+
+      // Actualiza el contador de favoritos en Firestore
+      await updateDoc(propertyDocRef, {
+        favoriteCount: isFavorite ? decrement(1) : increment(1),
+      });
+
+      // Resto del código de toggleFavorite
+    } catch (error) {
+      console.error("Error al actualizar favoritos en Firestore:", error);
+    }
+  };
+
   const applyFilters = () => {
     const filtered = propertyData.filter((property) => {
-      const isTypeMatch = filterType === 'all' || property.type === filterType;
+      const isTypeMatch = filterType === "all" || property.type === filterType;
       const isPriceMatch =
-        property.price >= parseInt(minPrice) && property.price <= parseInt(maxPrice);
-      const isStateMatch = filterState === 'all' || property.state === filterState;
+        property.price >= parseInt(minPrice) &&
+        property.price <= parseInt(maxPrice);
+      const isStateMatch =
+        filterState === "all" || property.state === filterState;
 
       return isTypeMatch && isPriceMatch && isStateMatch;
     });
@@ -133,24 +205,24 @@ const PropertyList = ({ onPropertyClick }) => {
               </Select>
             </Col>
             <Col xs={24} sm={12} md={6} lg={6}>
-            <Text strong>
-              <DollarOutlined /> Rango de Precio
-            </Text>
-            <div style={{ display: 'flex' }}>
-              <Input
-                placeholder="Mínimo"
-                value={minPrice}
-                onChange={handleMinPriceChange}
-                style={{ width: 'calc(50% - 8px)', marginRight: '8px' }}
-              />
-              <Input
-                placeholder="Máximo"
-                value={maxPrice}
-                onChange={handleMaxPriceChange}
-                style={{ width: 'calc(50% - 8px)' }}
-              />
-            </div>
-          </Col>
+              <Text strong>
+                <DollarOutlined /> Rango de Precio
+              </Text>
+              <div style={{ display: "flex" }}>
+                <Input
+                  placeholder="Mínimo"
+                  value={minPrice}
+                  onChange={handleMinPriceChange}
+                  style={{ width: "calc(50% - 8px)", marginRight: "8px" }}
+                />
+                <Input
+                  placeholder="Máximo"
+                  value={maxPrice}
+                  onChange={handleMaxPriceChange}
+                  style={{ width: "calc(50% - 8px)" }}
+                />
+              </div>
+            </Col>
             <Col xs={24} sm={12} md={6} lg={6}>
               <Text strong>
                 <EnvironmentOutlined /> Estado
@@ -200,7 +272,7 @@ const PropertyList = ({ onPropertyClick }) => {
             <Col xs={24} sm={12} md={8} lg={8} key={property.id}>
               <Card
                 className="property-card"
-                onClick={() => onPropertyClick(property.id)}
+                onClick={() => handlePropertyClick(property.id)}
               >
                 <Image
                   src={property.image}
@@ -280,6 +352,24 @@ const PropertyList = ({ onPropertyClick }) => {
                   >
                     Reserva
                   </Button>
+                  {userAuthenticated ? (
+                    <Button
+                      type="primary"
+                      icon={isFavorite ? <HeartFilled /> : <HeartOutlined />}
+                      onClick={() => toggleFavorite(property.id)}
+                    >
+                      {isFavorite
+                        ? "Quitar de Favoritos"
+                        : "Añadir a Favoritos"}
+                    </Button>
+                  ) : (
+                    <Link to="/login">
+                      <Button type="primary">
+                        Inicia sesión para guardar en favoritos
+                      </Button>
+                    </Link>
+                  )}
+
                   <Text strong style={{ fontSize: "1.5rem" }}>
                     $ {property.price}
                   </Text>
