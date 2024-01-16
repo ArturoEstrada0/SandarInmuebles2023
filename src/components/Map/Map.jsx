e10609e5d43e051ac684f99d4218a332be1a8893
ArturoEstrada0 <116518245+ArturoEstrada0@users.noreply.github.com>
2024-01-16 15:31:38 -0600

    marcador mapa, con radio de medio km

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,70 +1,98 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect } from 'react'
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
-import L from 'leaflet'
-import 'leaflet/dist/leaflet.css'
-import icon from 'leaflet/dist/images/marker-icon.png'
-import iconShadow from 'leaflet/dist/images/marker-shadow.png'
-import { Button } from 'antd'
-import { EnvironmentOutlined } from '@ant-design/icons'
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup, Circle, useMap } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import icon from "../../assets/img/icono2.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import { Button } from "antd";
+import { EnvironmentOutlined } from "@ant-design/icons";
 
-const position = [19.7028, -101.1927]
-const zoom = 14
+const zoom = 14;
+const iconSize = [130, 92];
+const circleRadius = 500; // Radio de la circunferencia en metros
 
 let DefaultIcon = L.icon({
   iconUrl: icon,
   shadowUrl: iconShadow,
-})
+  iconSize: iconSize,
+  shadowSize: [0, 0],
+});
 
-L.Marker.prototype.options.icon = DefaultIcon
+L.Marker.prototype.options.icon = DefaultIcon;
 
 function MapInvalidateSize() {
-  const map = useMap()
+  const map = useMap();
 
   useEffect(() => {
-    map.invalidateSize()
-  }, [])
+    map.invalidateSize();
+  }, [map]);
 
-  return null
+  return null;
 }
 
-function SetViewOnClick({ coords }) {
-  const map = useMap()
+function Map({ height, width, markerCoords }) {
+  const [map, setMap] = useState(null);
 
-  return (
-    <Button
-      type='primary'
-      icon={<EnvironmentOutlined />}
-      style={{
-        zIndex: 999,
-        bottom: 10,
-        left: 23,
-        position: 'absolute',
-        cursor: 'pointer',
-      }}
-      onClick={() => {
-        map.setView(coords, zoom)
-      }}>
-      Centrar
-    </Button>
-  )
-}
+  useEffect(() => {
+    if (map && markerCoords) {
+      // Centrar automáticamente el mapa cuando cambian las coordenadas
+      map.setView(markerCoords, zoom);
+    }
+  }, [markerCoords, map]);
 
-function Map({ height, width }) {
+  const MapSetViewButton = () => {
+    const map = useMap();
+
+    return (
+      <Button
+        type="primary"
+        icon={<EnvironmentOutlined />}
+        style={{
+          zIndex: 999,
+          bottom: 10,
+          left: 23,
+          position: "absolute",
+          cursor: "pointer",
+        }}
+        onClick={() => {
+          if (markerCoords) {
+            map.setView(markerCoords, zoom);
+          }
+        }}
+      >
+        Centrar
+      </Button>
+    );
+  };
+
   return (
     <MapContainer
-      center={position}
+      center={markerCoords}
       zoom={zoom}
-      style={{ height: height, width: width }}>
+      style={{ height: height, width: width }}
+      whenCreated={setMap}
+    >
       <TileLayer
         url='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
         attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> © CartoDB'
         maxZoom={19}
       />
-      <SetViewOnClick coords={position} />
+      {markerCoords && (
+        <>
+          <Marker position={markerCoords}>
+            <Popup>Ubicación</Popup>
+          </Marker>
+          <Circle
+            center={markerCoords}
+            radius={circleRadius}
+            pathOptions={{ color: 'blue', fillColor: 'blue', fillOpacity: 0.2 }}
+          />
+        </>
+      )}
       <MapInvalidateSize />
+      {markerCoords && <MapSetViewButton />}
     </MapContainer>
-  )
+  );
 }
-export default Map
+
+export default Map;
