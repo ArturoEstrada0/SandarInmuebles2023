cf15d6924d1e91b6f54de0432d6942ae4cc4fef4
ArturoEstrada0 <116518245+ArturoEstrada0@users.noreply.github.com>
2024-01-25 15:18:59 -0600

    firebase

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -7,14 +7,14 @@ import { getFirestore, collection, addDoc, doc, setDoc } from 'firebase/firestor
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAyRTZNRCwagZOh7byzwSfTzDSBoCpndFo",
-  authDomain: "sandarinmuebles.firebaseapp.com",
-  projectId: "sandarinmuebles",
-  storageBucket: "sandarinmuebles.appspot.com",
-  messagingSenderId: "898443516583",
-  appId: "1:898443516583:web:0138a248020172d9051982",
-  measurementId: "G-R8VCMHDJ3N"
-};
+  apiKey: 'AIzaSyB_fD7f4xSwHrrSs1TeZf_tY4GYULNSIsA',
+  authDomain: 'sandar-inmuebles.firebaseapp.com',
+  projectId: 'sandar-inmuebles',
+  storageBucket: 'sandar-inmuebles.appspot.com',
+  messagingSenderId: '1048623026714',
+  appId: '1:1048623026714:web:8b981112e772fa5a78b7a3',
+  measurementId: 'G-ZRT9VJ085L',
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
