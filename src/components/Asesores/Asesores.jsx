31b8311d85aae9c9f76cac9685d71f44d8e49bfb
AdrianUrtiz <116136643+AdrianUrtiz@users.noreply.github.com>
2024-02-26 21:29:00 -0600

    add mobile responsive to asesores

--- src/components/Asesores/Asesores.jsx
+++ src/components/Asesores/Asesores.jsx
@@ -1,33 +1,37 @@
-import React from 'react';
-import { Layout, Typography, Row, Col, Statistic, Divider } from 'antd';
+import { Layout, Typography, Divider } from 'antd'
 import asesores from '../../assets/img/asesores.jpg'
-import './Asesores.css'; // Asegúrate de tener un archivo de estilos para personalizar según tus necesidades
+import './Asesores.css' // Asegúrate de tener un archivo de estilos para personalizar según tus necesidades
 
-const { Content } = Layout;
-const { Title, Paragraph } = Typography;
+const { Content } = Layout
+const { Title } = Typography
 
 export default function Asesores() {
   return (
-    <Content style={{backgroundColor: "#ffff"}}>
-    <div className="asesores-container">
-      <div className="image-container">
-        {/* Aquí deberías poner la ruta de tu imagen */}
-        <img src={asesores} alt="Imagen de asesor" />
-      </div>
-      <div className="asesor-container">
-        {/*<h4>ASESORES</h4>*/}
-        <Title level={2} className="asesor-title">
-          Encuentra a tu Asesor
+    <Content style={{ backgroundColor: '#ffff', marginTop: '3rem' }}>
+      <div className='asesores-container'>
+        <div className='image-container'>
+          {/* Aquí deberías poner la ruta de tu imagen */}
+          <img src={asesores} alt='Imagen de asesor' />
+        </div>
+        <div className='asesor-container'>
+          {/*<h4>ASESORES</h4>*/}
+          <Title level={3} className='asesor-title'>
+            Encuentra a tu Asesor
           </Title>
-        <p>
-        Nuestros expertos en bienes raíces te proporcionarán asesoramiento profesional durante todo el proceso de compra o venta de propiedades. Estamos comprometidos en ayudarte a tomar decisiones informadas y estratégicas.
-        </p>
-        <Divider style={{ borderWidth: '2px', margin: '20px 0', marginLeft:'40px' }} />
-        <div className='button-asesor'>
-        <button>Contactar</button>
+          <p>
+            Nuestros expertos en bienes raíces te proporcionarán asesoramiento
+            profesional durante todo el proceso de compra o venta de
+            propiedades. Estamos comprometidos en ayudarte a tomar decisiones
+            informadas y estratégicas.
+          </p>
+          <Divider
+            style={{ borderWidth: '2px', margin: '20px 0', marginLeft: '40px' }}
+          />
+          <div className='button-asesor'>
+            <button>Contactar</button>
+          </div>
         </div>
       </div>
-    </div>
     </Content>
-  );
+  )
 }
